5ea3c7f476eea9c8c4bd1f804706729928a6864a
Leonhardqwq <1971475853@qq.com>
2026-02-22 14:44:53 +0800

    sun cin.get(); 	modified:   0 zombie type/sun/sun.cpp

--- 0 zombie type/sun/sun.cpp
+++ 0 zombie type/sun/sun.cpp
@@ -1,8 +1,10 @@
 #include "../../inc/sun_model.hpp"
+#include <iostream>
 
 int main(){
     SunConfig config("sun_config.json");    
     // config.show_info();
     work(config);
+    // std::cin.get();
     return 0;
 }
